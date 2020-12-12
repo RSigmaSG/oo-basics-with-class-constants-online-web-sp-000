658c96baa08106cdc727fc95f642ba59d616bc63
Sumer   Gaikwad <sg.comparch@gmail.com>
2020-12-12 22:08:50 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -6,9 +6,9 @@ class Shoe
   
   def initialize(brand)
     @brand = brand
-    if(!BRAND.include(brand))
+    #if(!BRAND.include(brand))
       BRANDS << brand 
-    end
+    #end
   end
   
   def cobble
